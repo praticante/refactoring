dacabcdc7545be1699ae9bd074900003e281fec4
Francesco Bianco <info.francescobianco@gmail.com>
2019-09-16 08:45:06 +0200

    parrot class

---
+++ src/Parrot.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Praticante\Refactoring;
+
+class Parrot
+{
+    protected $flyPlan = [];
+
+    public function setupFlyPlan($flys)
+    {
+        foreach ($flys as $fly) {
+            $flyCode = substr(strtoupper($fly), 0, 3);
+            $flyPlaces = explode(',', $fly);
+            $flySpecial = strlen($fly) > 20;
+
+            $flyPlan[] = $flyCode . ':' . $flyPlaces[1] . '->' . $flyPlaces[2] . ($flySpecial ? '(S)' : '');
+        }
+    }
+}
